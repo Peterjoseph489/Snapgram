85349ae03e3cbb0866997dc14d14b5ac264427e9
Peter Joseph <josephpeter489@gmail.com>
2023-11-10 17:20:42 +0100

    continous learning

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,17 @@
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+
+
 const SignupForm = () => {
+  const formSchema = z.object({
+    username: z.string().min(2).max(50),
+  })
+  
   return (
-    <div>SignupForm</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
