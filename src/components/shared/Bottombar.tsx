6f88d37220d50e2aba9885e284b8330d15a79c9c
Peter Joseph <josephpeter489@gmail.com>
2023-11-13 12:28:51 +0100

    continous learning

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,3 +1,4 @@
+import { bottombarLinks } from '@/constants';
 import { Link, useLocation } from 'react-router-dom'
 
 const Bottombar = () => {
@@ -5,7 +6,27 @@ const Bottombar = () => {
 
   return (
     <section className='bottom-bar'>
-
+      {bottombarLinks.map((link) => {
+        const isActive =pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label} 
+            className={`bottombar-link group ${
+            isActive && 'bg-primary-500'
+            }`}
+          >
+            <img 
+              src={link.imgURL}
+              alt={link.label}
+              className={`group-hover:invert-white ${
+              isActive && 'invert-white'
+              }`}
+            />
+              {link.label}
+          </Link>
+        )
+      })}
     </section>
   )
 }
