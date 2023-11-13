da0e83663caa02006ee1abdb676f8ca93d91e30b
Peter Joseph <josephpeter489@gmail.com>
2023-11-13 14:12:30 +0100

    continous learning

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react'
 import { FileWithPath, useDropzone } from "react-dropzone";
-import { Button } from "@/components/ui";
+import { Button } from "../ui/button";
 
 type FileUploaderProps = {
     fieldChange: (files: File[]) => void;
@@ -32,9 +32,16 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
       <input {...getInputProps()}  className='cursor-pointer'/>
       {
         fileUrl ? (
-            <div>
-                test 1
-            </div>
+            <>
+                <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+                    <img 
+                        src={fileUrl}
+                        alt='image'
+                        className='file_uploader-img'
+                    />
+                </div>
+                <p className='file_uploader-label'>Click or drag photo to replace</p>
+            </>
         ) : (
             <div className='file_uploader-box'>
                 <img 
