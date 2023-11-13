ebd7f114183f7f00a51bd37b746fbda12cbab31f
Peter Joseph <josephpeter489@gmail.com>
2023-11-13 12:20:54 +0100

    continous learning

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const Bottombar = () => {
+  const { pathname } = useLocation();
+
   return (
-    <div>Bottombar</div>
+    <section className='bottom-bar'>
+
+    </section>
   )
 }
 
