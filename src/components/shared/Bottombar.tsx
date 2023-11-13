ab1eb32080b06e8a74f56f4ee0b22d4c09021783
Peter Joseph <josephpeter489@gmail.com>
2023-11-13 12:47:16 +0100

    continous learning

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -12,18 +12,18 @@ const Bottombar = () => {
           <Link
             to={link.route}
             key={link.label} 
-            className={`bottombar-link group ${
-            isActive && 'bg-primary-500'
-            }`}
+            className={`${
+            isActive && 'bg-primary-500 rounded-[10px]'
+            } flex-center flex-col gap-1 p-2 transition`}
           >
             <img 
               src={link.imgURL}
               alt={link.label}
-              className={`group-hover:invert-white ${
-              isActive && 'invert-white'
-              }`}
+              width={16}
+              height={16}
+              className={`${isActive && 'invert-white'}`}
             />
-              {link.label}
+              <p className='tiny-medium text-light-2'>{link.label}</p>
           </Link>
         )
       })}
