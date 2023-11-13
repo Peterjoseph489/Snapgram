6936114a48ce23b31658825e9d34c6157d021802
Peter Joseph <josephpeter489@gmail.com>
2023-11-13 12:48:34 +0100

    continous learning

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -13,6 +13,9 @@ const CreatePost = () => {
           />
           <h2 className='h3-bold md:h2-bold text-left w-full'>Create Post</h2>
         </div>
+
+
+        
       </div>
     </div>
   )
