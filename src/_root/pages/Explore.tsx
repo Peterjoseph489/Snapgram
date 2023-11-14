4182955d8084bf19f5cd33d6b38776ee7871b20d
Peter Joseph <josephpeter489@gmail.com>
2023-11-14 11:42:50 +0100

    continous learning

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import { Input } from '@/components/ui/input'
+
+
 
 const Explore = () => {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
-    <div>Explore</div>
+    <div className='explore-container'>
+      <div className='explore-inner_container'>
+        <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
+        <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
+          <img 
+            src='/assets/icons/search.svg'
+            width={24}
+            height={24}
+            alt='search'
+          />
+          <Input 
+            type='text'
+            placeholder='Search'
+            className='explore-search'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
